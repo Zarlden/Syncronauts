2459409dba7fb7bb0f921b4ff19221bf4a0ff921
dotori0409 <dotori0409@gmail.com>
2023-04-20 17:42:18 +1000

    resolved conflict

---
+++ Interactable_Objects/Barrier/Barrier.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+const colourNode = preload("res://ColourNode.gd")
+var barrier_colour = colourNode.colourSet.BLUE
+
+func _ready():
+	$CollisionShape2D.set_deferred("disabled", false)
+	barrier_colour = $ColourNode.colour
+
+func _on_body_entered(body):
+	if body is Player:
+		if body.colour == barrier_colour:
+			$BarrierShape/CollisionShape2D.set_deferred("disabled", true)
