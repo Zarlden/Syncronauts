0c5faf7cd70020df1035c3f671b3e77e4c3c2880
Damon Lam <damon.lam.au@gmail.com>
2023-04-27 15:47:23 +1000

    Working with portal timeout

--- Interactable_Objects/portal/portal.gd
+++ Interactable_Objects/portal/portal.gd
@@ -5,6 +5,8 @@ extends Node2D
 
 var p1_pos = null
 var p2_pos = null
+var portal_lock = false
+@onready var timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,18 +14,26 @@ func _ready():
 
 	p1_pos = $Portal1.get_position()
 	p2_pos = $Portal2.get_position()
-	print(p1_pos, p2_pos)
 
+
+func teleport(player, to):
+	if not portal_lock:
+		if not portal_lock:
+			player.set_position(to)
+		timer.start()
+		portal_lock = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if $Portal1.has_overlapping_bodies():
+		for node in $Portal1.get_overlapping_bodies():
+			if node is Player:
+				teleport(node, p2_pos)
+	elif $Portal2.has_overlapping_bodies():
+		for node in $Portal2.get_overlapping_bodies():
+			if node is Player:
+				teleport(node, p1_pos)
 
-func _on_portal_1_body_entered(body):
-	if body is Player:
-		body.set_position(p2_pos)
 
-
-func _on_portal_2_body_entered(body):
-	if body is Player:
-		body.set_position(p1_pos)
+func _on_timer_timeout():
+	portal_lock = false
