80f3c31cc87b68ed7cb789f603c0a4cfc7681fd2
Damon Lam <damon.lam.au@gmail.com>
2023-07-20 13:23:40 +1000

    Removal and adding of children with pressure plate

--- Interactable_Objects/pressure_plate_group/pressure_plate_group.gd
+++ Interactable_Objects/pressure_plate_group/pressure_plate_group.gd
@@ -2,10 +2,27 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
+var children = []
+
 func _ready():
-	pass # Replace with function body.
+	for node in get_children():
+		if node != $"Pressure Plate":
+			children += node
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+
+func _on_pressure_plate_pressure_plate_released():
+	for child in children:
+		if child:
+			remove_child(child)
+
+
+func _on_pressure_plate_pressure_plate_pressed():
+	for child in children:
+		if not child:
+			add_child(child)
