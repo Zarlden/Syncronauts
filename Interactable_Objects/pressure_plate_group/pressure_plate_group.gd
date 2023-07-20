3e458b614910c7dac379c4f207ed68069d319f8f
Damon Lam <damon.lam.au@gmail.com>
2023-07-20 18:07:51 +1000

    Added todo

--- Interactable_Objects/pressure_plate_group/pressure_plate_group.gd
+++ Interactable_Objects/pressure_plate_group/pressure_plate_group.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 
+##TODO want to apply function to all children -- initially wanted color cascade but maybe nvm
+
 # Called when the node enters the scene tree for the first time.
 var children = []
 
@@ -9,6 +11,14 @@ func _ready():
 		if node != $"Pressure Plate":
 			children += node
 
+func filter_red():
+	for child in children:
+		if child is Barrier:
+			if child.barrier_colour == $ColourNode.colourSet.RED:
+				remove_child(child)
+			elif child.barrier_colour == $ColourNode.colourSet.PURPLE:
+				remove_child(child)
+				add_child()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- Level/World.gd
+++ Level/World.gd
@@ -1,4 +1,5 @@
 extends Node2D
+class_name Barrier
 
 @onready var camera = $Camera2D
 @onready var spawn = $SpawnPosition
